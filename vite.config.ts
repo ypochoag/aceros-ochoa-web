f0bd2308be56114bd97a4d098d621cba64f271b1
Yuri Ochoa <ypochoag@gmail.com>
2026-01-31 21:25:05 -0500

    fix url base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base:'/aceros-ochoa-web/',
+  base:'/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
