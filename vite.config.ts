63417cae7625034d90a52ec0cf320ee420a15748
Yuri Ochoa <97069512+ypochoag@users.noreply.github.com>
2026-01-31 22:45:29 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base:'/',
+  base:'https://www.acerosochoaguerrero.lat',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
