2aa50b99da2de2d43632e0f5b7e03b03db5e655a
Yuri Ochoa <ypochoag@gmail.com>
2026-01-31 21:09:41 -0500

    Fix routing for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base:'/aceros-ochoa-web/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
